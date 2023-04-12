05452c108270e3356619555e656c09d7fe203c06
EthanMuchnik25 <emuchnik@andrew.cmu.edu>
2023-04-12 05:41:42 -0400

    brought to you by ligma corporation

--- Python/pokemon.py
+++ Python/pokemon.py
@@ -12,8 +12,8 @@ FirstEvol = 50 # XP needed to evolve
 
 LastEvol = 100 #XP needed to evolve
 
-evolutionStatBoost = {"charmander": [20,20,20,20], "charmeleon": [20,20,20,20], "squirtle": [20,20,20,20], "wartortle": [20,20,20,20],
-                       "bulbasaur": [20,20,20,20], "ivysaur": [20,20,20,20], "pikachu": [20,20,20,20]}
+evolutionStatBoost = {"charmander": [20,20,20,20,20], "charmeleon": [20,20,20,20,20], "squirtle": [20,20,20,20,20], "wartortle": [20,20,20,20,20],
+                       "bulbasaur": [20,20,20,20,20], "ivysaur": [20,20,20,20,20], "pikachu": [20,20,20,20,20]}
 
 baseStats = {"charmander": [0,0,0,0,0], "bulbasaur": [0,0,0,0,0], "squirtle": [0,0,0,0,0], "pikachu": [0,0,0,0,0], "raichu": [0,0,0,0,0], "ivysaur": [0,0,0,0,0], "charmeleon": [0,0,0,0,0], "wartortle": [0,0,0,0,0]}
 
