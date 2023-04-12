caa123d15dcf37ff478a5097f538fc6f69099a13
EthanMuchnik25 <emuchnik@andrew.cmu.edu>
2023-04-12 06:29:44 -0400

    work2

--- Python/breakScript.py
+++ Python/breakScript.py
@@ -33,11 +33,12 @@ def mainFunc(secondQueue):
     GPIO.setup(BEAM_PIN2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(BEAM_PIN3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(BEAM_PIN4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(BEAM_PIN5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(BEAM_PIN, GPIO.FALLING, callback=break_beam_callback)
     GPIO.add_event_detect(BEAM_PIN2, GPIO.FALLING, callback=break_beam_callback)
     GPIO.add_event_detect(BEAM_PIN3, GPIO.FALLING, callback=break_beam_callback)
     GPIO.add_event_detect(BEAM_PIN4, GPIO.FALLING, callback=break_beam_callback)
-
+    GPIO.add_event_detect(BEAM_PIN5, GPIO.FALLING, callback=break_beam_callback)
     while True:
         if returnPin != -1:
             print("omg wadi")
